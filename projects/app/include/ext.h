bec7a771ad10d7e14ebf86a09f06972f49a7ec3d
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-08-25 13:36:54 +0200

    Improve example

---
+++ projects/app/include/ext.h
@@ -0,0 +1,8 @@
+#ifndef APP_EXT_H
+#define APP_EXT_H
+
+#include <stdint.h>
+
+int32_t add(int32_t a, int32_t b);
+
+#endif
--- projects/app/src/main.c
+++ projects/app/src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ext.h>
 
 int main()
 {
-    printf("hello world!\n");
+    int32_t c = add(1, 2);
+    printf("%d\n", c);
     return EXIT_SUCCESS;
 }
